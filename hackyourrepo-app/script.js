ba67706e8a604af43db6680c5ef0ab0a02a59060
obada othman <obada.oth@gmail.com>
2020-08-30 02:23:22 +0200

    added contributors placeholder data to project hackyourrepo and displayed it in the app

--- hackyourrepo-app/script.js
+++ hackyourrepo-app/script.js
@@ -6,9 +6,10 @@
 
 const selectRepo = document.getElementById('select-repo');
 const repoDetails = document.getElementById('details');
+const contributorsList = document.getElementById('contributors-list');
 function getDetails(event) {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'placeholders.json', true);
+  xhr.open('GET', 'placeholders.json');
   xhr.onload = () => {
     if (xhr.status === 200) {
       const repos = JSON.parse(xhr.responseText);
@@ -21,7 +22,7 @@ function getDetails(event) {
           '<tr>' +
           '<td class="titles"> Repository: </td>' +
           '<td class="content">' +
-          '<a href="" targer="#">' +
+          '<a href="" target="#">' +
           repos[i].name +
           '</a>' +
           '</td>' +
@@ -45,7 +46,6 @@ function getDetails(event) {
           '</td>' +
           '</tr>' +
           '</table>';
-
         repoDetails.innerHTML = event.target.value;
       }
     }
@@ -53,5 +53,42 @@ function getDetails(event) {
 
   xhr.send();
 }
-document.addEventListener('load', getDetails);
+function getContributors(event) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'placeholders.json');
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      const repos = JSON.parse(xhr.responseText);
+      const options = Array.from(document.getElementsByTagName('option'));
+      for (let i in options) {
+        options[i].value = '';
+
+        options[i].value +=
+          '<ul>' +
+          '<li>' +
+          '<img src="' +
+          repos[i].contributor1.img +
+          '" width="40" class="cont-img">' +
+          '<a href="" target=#>' +
+          repos[i].contributor1.name +
+          '</a>' +
+          '</li>' +
+          '<li>' +
+          '<img src="' +
+          repos[i].contributor2.img +
+          '" width="40" class="cont-img">' +
+          '<a href="" target=#>' +
+          repos[i].contributor2.name +
+          '</a>' +
+          '</li>' +
+          '</ul>';
+
+        contributorsList.innerHTML = event.target.value;
+      }
+    }
+  };
+  xhr.send();
+}
+
 selectRepo.addEventListener('change', getDetails);
+selectRepo.addEventListener('change', getContributors);
